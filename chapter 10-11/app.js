4646c8fdc08329ff645749f6d3a49f0648a38ff7
Areeba naz farooqui <areebanaz4567@gmail.com>
2024-05-12 14:44:58 +0500

    javascript chapters

---
+++ chapter 10-11/app.js
@@ -0,0 +1,72 @@
+// chap 10 USER INPUT & CONDITIONAL STATEMENT
+// 1. Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+var userCity = prompt("Enter your city name");
+if (userCity === "Karachi") {
+  alert("Welcome to City of Lights!!");
+}
+
+// 2. Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+var userGender = prompt("Specify your Gender", "Male,Female");
+if (userGender === "Male") {
+  alert("Good Morning Sir!!");
+}
+if (userGender === "Female") {
+  alert("Good Morning Ma'am!!");
+}
+// 3. Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+var signalColor = prompt(
+  "Choose your road traffic signal color",
+  "Red, Yellow, Green"
+);
+if (signalColor === "Red") {
+  alert("Red!! Must Stop🤚");
+}
+if (signalColor === "Yellow") {
+  alert("Ready to Move!!🚶‍♂️");
+}
+if (signalColor === "Green") {
+  alert("Move Now!!🏃‍♂️");
+}
+// 5. Run this script, & check whether alert message would be
+// displayed or not. Record the outputs.
+var a = 4;
+if (++a === 5) {
+  alert("given condition for variable a is true");
+}
+var b = 82;
+if (b++ === 83) {
+  alert("given condition for variable b is true");
+}
+var c = 12;
+if (c++ === 13) {
+  alert("condition 1 is true");
+}
+if (c === 13) {
+  alert("condition 2 is true");
+}
+if (++c < 14) {
+  alert("condition 3 is true");
+}
+if (c === 14) {
+  alert("condition 4 is true");
+}
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost) {
+  alert("The cost equals");
+}
+if (true) {
+  alert("True");
+}
+if (false) {
+  alert("False");
+}
+if ("car" < "cat") {
+  alert("car is smaller than cat");
+}
---
+++ chapter 3/app.js
@@ -0,0 +1,33 @@
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+var age = "18 years old";
+alert("I am "+age);
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+var vistingNum = "You have visited this site 19 times";
+alert(vistingNum);
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+var birthYear = 2004;
+document.write("<strong>My birth year is</strong> "+ birthYear + "<br>" ); 
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+var visitorName = "John Doe" ;
+var productTitle = "T-Shirt(s)";
+var quantity = "5";
+
+document.write("\n" + "<strong>"+ visitorName+ "</strong>" + " ordered " + "<strong>" + quantity + productTitle +"</strong>" + " on XYZ Clothing store");
+
+// END OF CHAPTER 3 
